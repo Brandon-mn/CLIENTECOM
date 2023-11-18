a0b2b562d371d5d8a1be1254259dfae3c4deec5f
costi <costi@DESKTOP-886NS64>
2023-11-18 17:53:52 -0600

    Agregar archivos de proyecto.

---
+++ CLIENTECOM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLIENTECOM
+{
+     class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var servidorDCOM = COM_DCOM.SERVIDOR();
+                string resultado = servidorDCOM.ObtenerSaludo("UsuarioDCOM");
+                Console.WriteLine(resultado);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
